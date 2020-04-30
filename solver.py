edc9a2a57ff45818d36b49fe27fb69f4faceb378
rpalmaka <rpalmaka@berkeley.edu>
2020-04-29 18:34:10 -0700

    59 w brute dijk

--- solver.py
+++ solver.py
@@ -31,31 +31,54 @@ def solve(G):
     # we follow the original idea, creating a dominating set and building a tree from that
     min_dom_set = nx.algorithms.approximation.min_weighted_dominating_set(G, "weight")
     # print(min_dom_set)
-    print(min_dom_set)
-    return_set = set([]) # we create a set of nodes that we want to be in the final tree
+    # print(min_dom_set)
+    lists = []
+    # return_set = set([]) # we create a set of nodes that we want to be in the final tree
+    leest = list(min_dom_set)
     source = min_dom_set.pop()
 
+
     # we connect all the nodes in the dominating set by finding all the nodes in the shortest paths
-    for node in min_dom_set:
+    # for node in min_dom_set:
+    #     # use dijkstra shortest paths to create the new tree
+    #     curr_path = nx.algorithms.shortest_paths.dijkstra_path(G, source, node)
+    #     for node1 in curr_path:
+    #         return_set.add(node1)
+    for a in leest:
+        return_set = set([])
+        for b in leest:
+
         # use dijkstra shortest paths to create the new tree
-        curr_path = nx.algorithms.shortest_paths.dijkstra_path(G, source, node)
-        for node1 in curr_path:
-            return_set.add(node1)
+            curr_path = nx.algorithms.shortest_paths.dijkstra_path(G, a, b)
+            for node1 in curr_path:
+                return_set.add(node1)
+        lists.append(return_set)
 
     # checks if nodes were added to return set, if none there is a central node and return single node
-    print(len(return_set))
-    if not return_set:
+    # print(len(return_set))
+    if not lists:
         r = nx.Graph()
         r.add_node(source)
         return r
 
+
+    min_tree = source
+    min = 10000000
+    for c in lists:
+        min_dom_subgraph = G.subgraph(list(c))
+        min_dom_tree = nx.minimum_spanning_tree(min_dom_subgraph)
+        pair = average_pairwise_distance(min_dom_tree)
+        if pair < min:
+            min = pair
+            min_tree = min_dom_tree
+
+
     # we recreate the subgraph with the necessary nodes to keep it connected
-    min_dom_subgraph = G.subgraph(list(return_set))
+    # min_dom_subgraph = G.subgraph(list(return_set))
     # resulting graph will have extra unnecessary edges, use MST to prune
-    min_dom_tree = nx.minimum_spanning_tree(min_dom_subgraph)
-    # print(min_dom_tree.nodes)
-    # print(min_dom_tree[0][1]['weight'])
-    return min_dom_tree
+    # min_dom_tree = nx.minimum_spanning_tree(min_dom_subgraph)
+
+    return min_tree
 
     """
     #MST baseline
@@ -64,28 +87,28 @@ def solve(G):
     """
 
 #
-# if __name__ == "__main__":
-#     output_dir = "outputs"
-#     input_dir = "inputs"
-#     for input_path in os.listdir(input_dir):
-#         graph_name = input_path.split(".")[0]
-#         G = read_input_file(f"{input_dir}/{input_path}")
-#         T = solve(G)
-#         print(graph_name)
-#         write_output_file(T, f"{output_dir}/{graph_name}.out")
+if __name__ == "__main__":
+    output_dir = "outputs"
+    input_dir = "inputs"
+    for input_path in os.listdir(input_dir):
+        graph_name = input_path.split(".")[0]
+        G = read_input_file(f"{input_dir}/{input_path}")
+        T = solve(G)
+        print(graph_name)
+        write_output_file(T, f"{output_dir}/{graph_name}.out")
 
 
 # """
-if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    # graph_name = path.split(".")[0] --> doesn't work
-    G = read_input_file("inputs/" +path)
-    T = solve(G)
-    assert is_valid_network(G, T)
-    print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-    # need to change to be dynamic for file name
-    write_output_file(T, f"myoutputs/test.out")
+# if __name__ == '__main__':
+#     assert len(sys.argv) == 2
+#     path = sys.argv[1]
+#     # graph_name = path.split(".")[0] --> doesn't work
+#     G = read_input_file("inputs/" +path)
+#     T = solve(G)
+#     assert is_valid_network(G, T)
+#     print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+#     # need to change to be dynamic for file name
+#     write_output_file(T, f"myoutputs/test.out")
 # """
 
 
